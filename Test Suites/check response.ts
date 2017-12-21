9b4771d0e5c8740a40144c3455af5ae288dd2d2d
Gennadii Miroshnichenko <genamister@gmail.com>
2017-12-21 16:51:51 +0200

    add test for status code

---
+++ Test Suites/check response.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>check response</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>117c2b10-484a-4829-8a16-5a5518328479</testSuiteGuid>
+   <testCaseLink>
+      <guid>8cdc2363-61dd-42ad-bbf0-d3bb0b0b2dc2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Check response status code</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
